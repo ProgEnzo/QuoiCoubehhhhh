f3e47a1905c7804c3d575ce3d6a37f7327e63134
Aloisz <a.tupinon@rubika-edu.com>
2023-05-27 17:54:50 +0200

    qsdµ

--- JamV2_2023/Assets/Script/Camera/CameraManager.cs
+++ JamV2_2023/Assets/Script/Camera/CameraManager.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-
+    
     public CinemachineTargetGroup targetGroup;
 
+    public int count;
+    public List<Transform> placesToSpawns;
+    
 
     public static CameraManager instance;
     private void Awake()
@@ -35,5 +38,6 @@ public class CameraManager : MonoBehaviour
         Transform p = GameObject.Find("Player(Clone)").transform;
         p.name = "Player";
         targetGroup.AddMember(p, 1,1); 
+        count
     }
 }
